4835c73888cba73bdde684b8d8dcb2bffcd85068
Zach Walker <zachscottwalker@gmail.com>
2019-04-24 12:50:32 -0400

    add test for range 7900 to 7920, passing

--- src/test/java/com/zachwalker/PrimeNumberCalculatorTest.java
+++ src/test/java/com/zachwalker/PrimeNumberCalculatorTest.java
@@ -37,5 +37,16 @@ public class PrimeNumberCalculatorTest {
 		Assert.assertTrue(primeList.size() == 4);
 		
 	}
+	
+	@Test
+	public void generating_7900_to_7920_returns_7901_7907_7919() {
+		
+		List<Integer> primeList = testPNG.generate(7900, 7920);
+		
+		Assert.assertTrue(primeList.contains(7901));
+		Assert.assertTrue(primeList.contains(7907));
+		Assert.assertTrue(primeList.contains(7919));
+		
+	}
 
 }
